99071bf53761486cb72404e56bba563e8fe53406
Dmitry Petrov <petrov6827@gmail.com>
2018-12-17 03:48:01 +0300

    фото и видео галерея готовы, дальше - страница отзывов зачем дизайнер делал фото первой картинки видео, и стоит ли их верстать с свг кнопками управления видосом?

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 var Toggle = document.querySelector('.main-nav__toggle');
 var Close = document.querySelector('.main-nav__close');
 var Menu = document.querySelector('.menu');
+var Triangle = document.querySelector('.main__triangle');
 
 Toggle.addEventListener('click',function(event) {
     event.preventDefault();
     Menu.classList.add('menu--show');
+    Triangle.classList.add('main__triangle--close');
 });
 
 Close.addEventListener('click',function(event) {
     event.preventDefault();
     Menu.classList.remove('menu--show');
+    Triangle.classList.remove('main__triangle--close');
 });
 
 
